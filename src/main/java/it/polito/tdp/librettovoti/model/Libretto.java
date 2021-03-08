b25456d9f2a50ffab898dedb9ff0de5b8b0b15ce
Silvio Tanzarella <s259943@studenti.polito.it>
2021-03-08 11:23:17 +0100

    Soluzione punto 2

--- src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
+++ src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
@@ -16,6 +16,41 @@ public class Libretto {
 		this.voti.add(v);
 	}
 	
+	/* queste due funzioni FANNO SCHIFO
+	public void stampaVotiUguali(int punteggio) {
+		//il libretto stampa da solo i voti
+	}
+	
+	public String votiUguali(int punteggio) {
+		//calcola una stringa che contiene i voti
+		//sarà poi il chiamante a stamparli
+		//-> solo il nome?
+		//->tutto il voto.toString()
+	}
+	*/
+	public List<Voto> listaVotiUguali(int punteggio){
+		//espone all'esterno il tipo di struttura interna utilizzata
+		List<Voto> risultato = new ArrayList<>();
+		for(Voto v:this.voti) {
+			if(v.getVoto() == punteggio) {
+				risultato.add(v);
+			}
+		}
+		return risultato;
+	}
+	
+	
+	public Libretto votiUguali(int punteggio) {
+		Libretto risultato = new Libretto();
+		for(Voto v:this.voti) {
+			if(v.getVoto() == punteggio) {
+				risultato.add(v);
+			}
+		}
+		return risultato;
+	}
+	
+	
 	public String toString() {
 		String s = "";
 		for(Voto v: this.voti) {
